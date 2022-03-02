533838b937af0b59f32f8eac0b609ff80fe7cd09
HENRY-KC <henrykc24@yahoo.com>
2022-03-02 11:30:02 +0100

    Set up the intitial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    Lease a home front end
+    <button type="button" className="btn btn-primary">
+      Primary
+    </button>
+  </div>
+);
+
+export default App;
