5baa165c14b07772ffed972dcfd5456831cfc741
HENRY-KC <henrykc24@yahoo.com>
2022-03-03 11:55:54 +0100

    Implement email handling

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -40,6 +40,31 @@ const Login = () => {
     return true;
   };
 
+  return (
+    <form className={cx(colorScheme.blue, form)}>
+      <div style={{ backgroundColor: colorScheme.blue }} className={cx(formHeader)}>
+        {/* <h2 className={h2}>Login</h2> */}
+        <p className={p}>Fill out this form to log in!</p>
+      </div>
+
+      <div className={cx('form-group', formGroup)}>
+        <label style={{ color: colorScheme.textPale }} className={label} htmlFor="email">
+          Email Address
+          <span> *</span>
+          <input
+            onChange={handleInput}
+            value={input.email}
+            type="email"
+            className={cx('form-control', formControl)}
+            name="email"
+            id="email"
+            required="required"
+          />
+        </label>
+      </div>
+
+    </form>
+  );
 };
 
 export default Login;
