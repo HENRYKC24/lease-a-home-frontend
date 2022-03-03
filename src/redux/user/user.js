5cd36ba7949b686d3100e8ff103b24dd9a5fc650
HENRY-KC <henrykc24@yahoo.com>
2022-03-03 11:51:24 +0100

    Add action creators

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -12,3 +12,19 @@ const initialState = {
 const SIGN_UP = 'lease_a_home/SIGN_UP';
 const LOGIN = 'lease_a_home/LOGIN';
 const LOGOUT = 'lease_a_home/LOGOUT';
+
+// Action Creators
+export const signUp = (payload) => ({
+  type: SIGN_UP,
+  payload,
+});
+
+export const login = (payload) => ({
+  type: LOGIN,
+  payload,
+});
+
+export const logout = (payload) => ({
+  type: LOGOUT,
+  payload,
+});
