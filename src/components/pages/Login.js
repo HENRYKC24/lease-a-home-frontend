a6971ae18f0e8ab9a68f9f0ecc4502eaa5996824
HENRY-KC <henrykc24@yahoo.com>
2022-03-03 11:54:59 +0100

    Import styling object and implement basic local functions

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -4,7 +4,42 @@ import colorScheme from '../../colorScheme';
 import style from './signup.module.css';
 
 const Login = () => {
-  
+  const {
+    formHeader,
+    form,
+    formGroup,
+    formControl,
+    label,
+    btn,
+    // h2,
+    p,
+    or,
+    line,
+    orGroup,
+  } = style;
+
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    const { email, password } = input;
+    if (password && email) {
+      event.preventDefault();
+      console.log(input, 'input state>>>>');
+    }
+    return true;
+  };
+
 };
 
 export default Login;
