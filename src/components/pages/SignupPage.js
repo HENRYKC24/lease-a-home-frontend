e5e3683083bccf6e8351e62e568baacb35ba269b
HENRY-KC <henrykc24@yahoo.com>
2022-03-03 11:59:03 +0100

    Add basic sign up structure

---
+++ src/components/pages/SignupPage.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { NavLink } from 'react-router-dom';
+import cx from 'classnames';
+import colorScheme from '../../colorScheme';
+import style from './signup.module.css';
+import hitAPIWithSignupDetails from '../../redux/user/user';
+
+const SignupPage = () => {
+  
+};
+
+export default SignupPage;
