be4f6a97ef5fbdb20f5fac268cd069d882d3987c
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-03-03 11:03:52 +0200

    Create file for links

---
+++ src/helpers/links.js
@@ -0,0 +1,28 @@
+const links = [
+  {
+    id: 1,
+    path: '/',
+    text: 'APARTMENTS',
+    name: 'apartments',
+  },
+  {
+    id: 2,
+    path: '/lease_form',
+    text: 'LEASE FORM',
+    name: 'lease_form',
+  },
+  {
+    id: 3,
+    path: '/my_leases',
+    text: 'MY LEASES',
+    name: 'my_leases',
+  },
+  {
+    id: 4,
+    path: '/delete_leases',
+    text: 'DELETE LEASES',
+    name: 'delete_leases',
+  },
+];
+
+export default links;
