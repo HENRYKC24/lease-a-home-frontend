dbf75b9ccf2d8edb3ae9db66e885ec0be6bbca52
HENRY-KC <henrykc24@yahoo.com>
2022-03-03 14:39:38 +0100

    Add other pages to the routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Provider } from 'react-redux';
-import SignupPage from './components/pages/SignupPage';
-// import Login from './components/pages/Login';
-import store from './redux/configureStore';
-
-const App = () => (
-  <Provider store={store}>
-    <SignupPage />
-  </Provider>
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignupPage from './components/pages/SignupPage';
+import Login from './components/pages/Login';
+import store from './redux/configureStore';
 import NavBar from './components/navbar/NavBar';
 import Apartments from './pages/Apartments';
 import LeaseForm from './pages/LeaseForm';
 import MyLeases from './pages/MyLeases';
 import DeleteLeases from './pages/DeleteLeases';
+// require('dotenv').config()
 
 const App = () => (
   <Router>
-    <NavBar />
-    <Routes>
-      <Route exact path="/" element={<Apartments />} />
-    </Routes>
-    <Routes>
-      <Route exact path="/lease_form" element={<LeaseForm />} />
-    </Routes>
-    <Routes>
-      <Route exact path="/my_leases" element={<MyLeases />} />
-    </Routes>
-    <Routes>
-      <Route exact path="/delete_leases" element={<DeleteLeases />} />
-    </Routes>
+    <Provider store={store}>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Apartments />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/lease_form" element={<LeaseForm />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/sign_up" element={<SignupPage />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/my_leases" element={<MyLeases />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/delete_leases" element={<DeleteLeases />} />
+      </Routes>
+    </Provider>
   </Router>
->>>>>>> 9def4c50988b25143e4fbaff54c68ead1f8357f8
 );
 
 export default App;
