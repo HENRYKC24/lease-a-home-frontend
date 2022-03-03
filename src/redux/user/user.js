9913e6b7ad8ff9c852ccf5bdacfbfb2a35dfed02
HENRY-KC <henrykc24@yahoo.com>
2022-03-03 11:52:26 +0100

    Add middleware functions

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -44,3 +44,78 @@ export const userReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const hitAPIWithSignupDetails = (details) => async (dispatch) => {
+  const { name, email, password } = details;
+  try {
+    await axios({
+      method: 'post',
+      url: 'http://127.0.0.1:3000/users',
+      data: {
+        user: {
+          email,
+          password,
+          name,
+        },
+      },
+    });
+    dispatch(signUp({
+      name: '',
+      email: '',
+      loggenIn: false,
+      userId: '',
+      signedUp: 'up',
+    }));
+  } catch (error) {
+    dispatch(signUp({
+      name: '',
+      email: '',
+      loggenIn: false,
+      userId: '',
+      signedUp: 'down',
+    }));
+  }
+  // dispatch(fetchData(data));
+};
+
+export const hitAPIWithSigninDetails = (details) => async (dispatch) => {
+  const { email, password } = details;
+  try {
+    const signUpRespons = await axios({
+      method: 'post',
+      url: 'http://127.0.0.1:3000/users/sign_in',
+      data: {
+        user: {
+          email,
+          password,
+        },
+      },
+    });
+    const { data, headers } = signUpRespons;
+    const { user } = data;
+    const { authorization } = headers;
+    const mainUser = {
+      name: user.name,
+      email: user.email,
+      id: user.id,
+      auth: authorization,
+    };
+
+    localStorage.setItem('userAuth', JSON.stringify(authorization));
+
+    const appState = {
+      ...mainUser,
+    };
+    delete appState.auth;
+    dispatch(signUp({
+      ...appState,
+      loggenIn: false,
+    }));
+  } catch (error) {
+    console.log('Something went wront');
+  }
+
+  // dispatch(fetchData(data));
+};
+
+export default hitAPIWithSignupDetails;
