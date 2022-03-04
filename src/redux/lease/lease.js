b16af13076d9e9dda8a73086a18dcafc08563032
wuyepabdul <wuyepabdul@gmail.com>
2022-03-04 12:00:40 +0100

    Create reducer constants for leases

---
+++ src/redux/lease/lease.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const GET_LEASES_REQUEST = 'GET_LEASES_REQUEST';
+const GET_LEASES_SUCCESS = 'GET_LEASES_SUCCESS';
+const GET_LEASES_FAIL = 'GET_LEASES_FAIL';
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
+
