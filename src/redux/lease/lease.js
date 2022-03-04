ffe4732fc9ba13dc7fa0cb3fe47653fbb4c382fc
wuyepabdul <wuyepabdul@gmail.com>
2022-03-04 12:02:23 +0100

    Create getMyLeases action

--- src/redux/lease/lease.js
+++ src/redux/lease/lease.js
@@ -6,4 +6,14 @@ const GET_LEASES_FAIL = 'GET_LEASES_FAIL';
 
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
-
+export const getMyLeasesAction = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: GET_LEASES_REQUEST });
+    const { user } = getState();
+    console.log('user==>', user);
+    const { data } = await axios.get(`${baseUrl}/user/${user.id}/leases`);
+    console.log('data ==>', data);
+  } catch (error) {
+    dispatch({ type: GET_LEASES_FAIL, payload: error.message });
+  }
+};
