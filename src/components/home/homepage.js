b077c9238c9d8569af922ec80a9dcb8d699e4107
Whoistolu <digittolu25@gmail.com>
2022-03-04 08:56:21 +0100

    Return a map of the apartments array coming from the API

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -11,5 +11,24 @@ const Home = () => {
       <h1>Loading</h1>
     );
   }
- 
+  return (
+    <section>
+      <h1 className="apartment-heading text-center mt-3 text-uppercase">Latest Home</h1>
+      <div className="row">
+        {
+      apartments[0].map((item) => (
+        <div key={item.id} className="col-12 col-md-6 col-lg-4">
+          <Link style={{ textDecoration: 'none' }} to={`${item.id}`} className="m-3">
+            <img className="apartment-image center-block" src={item.image} alt={item.name} />
+            <h4 className="my-2 text-center apartmentname">{item.name}</h4>
+            <p className="description font-weight-light text-left ms-3">
+              {item.description}
+            </p>
+          </Link>
+        </div>
+      ))
+    }
+      </div>
+    </section>
+  );
 };
