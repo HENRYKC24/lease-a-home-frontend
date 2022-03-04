afb80ae36452d877ad3417de82548545ae93e1e2
Whoistolu <digittolu25@gmail.com>
2022-03-04 08:55:28 +0100

    Setup the home function using the arrow function in homepage.js

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+const Home = () => {
+  const apartmentss = useSelector((state) => state.apartment);
+  const { apartments } = apartmentss;
+  console.log(apartments, 'hello');
+  if (!apartments[0]) {
+    return (
+      <h1>Loading</h1>
+    );
+  }
+ 
+};
