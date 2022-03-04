f07fa910840d3a7ea934fe2c6d100687059a8f7c
Whoistolu <digittolu25@gmail.com>
2022-03-04 09:01:47 +0100

    Implement Reducers

--- src/redux/apartment/apartment.js
+++ src/redux/apartment/apartment.js
@@ -19,3 +19,21 @@ export const getOneApartment = (payload) => ({
   type: FETHCH_ONE_APARTMENT,
   payload,
 });
+
+// Reducers
+export const apartmentReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case FETHCH_APARTMENTS:
+      console.log(payload, state, 'this is payload');
+      return {
+        ...state,
+        apartments: [...state.apartments, payload],
+      };
+    case FETHCH_ONE_APARTMENT:
+      return payload;
+
+    default:
+      return state;
+  }
+};
