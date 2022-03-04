96a0ad292778a1070cf4610a3cb82cabc366f0ae
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-03-04 23:20:47 +0200

    Add lease reducer to configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { userReducer } from './user/user';
+import { leaseReducer } from './lease/lease';
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({
+  user: userReducer,
+  leaseReducer,
+});
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
