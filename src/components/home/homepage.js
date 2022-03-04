8cf78286b55b14bbdad36d47b9614024358ba2a8
Whoistolu <digittolu25@gmail.com>
2022-03-04 09:12:42 +0100

    Style homepage

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const Home = () => {
+export const Home = () => {
   const apartmentss = useSelector((state) => state.apartment);
   const { apartments } = apartmentss;
   console.log(apartments, 'hello');
+  console.log(Home);
   if (!apartments[0]) {
     return (
       <h1>Loading</h1>
@@ -32,3 +33,5 @@ const Home = () => {
     </section>
   );
 };
+
+export default Home;
