915755230cf8e268cdfaca396c07a1b067630927
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-03-05 01:52:55 +0200

    Add conditional to navigation panel

--- src/components/navbar/DesktopNavBar.js
+++ src/components/navbar/DesktopNavBar.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-const DesktopNavBar = ({ links, logo }) => (
-  <nav className="desktop-nav">
-    <div className="logo-container">
-      <img src={logo} alt="App logo" className="logo" />
-    </div>
-    <ul className="navLinks">
-      {links.map((link) => (
-        <li key={link.id} className={link.name}>
-          <NavLink exact="true" to={link.path}>
-            {link.text}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-    <div className="copyright">
-      <p>© 2022 ACHT.</p>
-    </div>
-  </nav>
-);
+const DesktopNavBar = ({ links, logo }) => {
+  const state = useSelector((state) => (state));
+  const { loggedIn } = state.user;
+
+  return (
+    <nav className="desktop-nav">
+      <div className="logo-container">
+        <img src={logo} alt="App logo" className="logo" />
+      </div>
+      <ul className="navLinks">
+        {
+          loggedIn === 'in' ? links.filter((link) => link.id < 6).map((userlinks) => (
+            <li key={userlinks.id > 5} className={userlinks.name}>
+              <NavLink exact="true" to={userlinks.path}>
+                {userlinks.text}
+              </NavLink>
+            </li>
+          ))
+            : links.map((link) => (
+              <li key={link.id > 5} className={link.name}>
+                <NavLink exact="true" to={link.path}>
+                  {link.text}
+                </NavLink>
+              </li>
+            ))
+        }
+      </ul>
+      <div className="copyright">
+        <p>© 2022 ACHT.</p>
+      </div>
+    </nav>
+  );
+};
 
 export default DesktopNavBar;
--- src/components/navbar/MobileNavBar.js
+++ src/components/navbar/MobileNavBar.js
@@ -1,37 +1,52 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { HiMenuAlt4 } from 'react-icons/hi';
 
-const MobileNavBar = ({ links, logo }) => (
-  <nav className="mobile navbar navbar-light fixed-top">
-    <div className="container-fluid">
-      <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-        <HiMenuAlt4 className="menu4" />
-      </button>
-      <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div className="offcanvas-header">
-          <div className="logo-container">
-            <img src={logo} alt="App logo" className="logo" />
+const MobileNavBar = ({ links, logo }) => {
+  const state = useSelector((state) => (state));
+  const { loggedIn } = state.user;
+
+  return (
+    <nav className="mobile navbar navbar-light fixed-top">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <HiMenuAlt4 className="menu4" />
+        </button>
+        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <div className="logo-container">
+              <img src={logo} alt="App logo" className="logo" />
+            </div>
+            <button type="button" className="btn-close text-reset close" data-bs-dismiss="offcanvas" aria-label="Close" />
           </div>
-          <button type="button" className="btn-close text-reset close" data-bs-dismiss="offcanvas" aria-label="Close" />
-        </div>
-        <div className="offcanvas-body align-self-center text-center">
-          <ul className="navLinks">
-            {links.map((link) => (
-              <li key={link.id} className={link.name} data-bs-toggle="offcanvas">
+          <div className="offcanvas-body align-self-center text-center">
+            <ul className="navLinks">
+              {
+          loggedIn === 'in' ? links.filter((link) => link.id < 6).map((userlinks) => (
+            <li key={userlinks.id > 5} className={userlinks.name}>
+              <NavLink exact="true" to={userlinks.path}>
+                {userlinks.text}
+              </NavLink>
+            </li>
+          ))
+            : links.map((link) => (
+              <li key={link.id > 5} className={link.name}>
                 <NavLink exact="true" to={link.path}>
                   {link.text}
                 </NavLink>
               </li>
-            ))}
-          </ul>
-          <div className="copyright">
-            <p>© 2022 ACHT.</p>
+            ))
+        }
+            </ul>
+            <div className="copyright">
+              <p>© 2022 ACHT.</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default MobileNavBar;
