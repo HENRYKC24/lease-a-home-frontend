dad6f230951d1a561428aaa2fc6b8443271728ef
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:19:37 +0100

    Add logout test file

---
+++ src/tests/authentication/logout.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Logout from '../../pages/Logout';
+import store from '../../redux/configureStore';
+
+it('Navbar renders the UI as expected', () => {
+  
+});
