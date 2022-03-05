a72a7d1aa81b739fd1f09caafa4c38857b0d6561
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:27:14 +0100

    Add sign in side effect test structure

---
+++ src/tests/authentication_reducers/__mocks__/hitAPIWithSigninDetails.js
@@ -0,0 +1,9 @@
+import { userReducer, login } from '../../../redux/user/user';
+// import { Dispatch } from "react";
+const hitAPIWithSigninDetails = (details) => {
+  const { name, email } = details;
+
+ 
+};
+
+export default hitAPIWithSigninDetails;
