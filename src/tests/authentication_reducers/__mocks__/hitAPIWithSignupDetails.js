9b8ef12849852a5a65ed1fb2cec711d3e240745a
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:28:42 +0100

    Implement sign up side effect test

--- src/tests/authentication_reducers/__mocks__/hitAPIWithSignupDetails.js
+++ src/tests/authentication_reducers/__mocks__/hitAPIWithSignupDetails.js
@@ -3,6 +3,13 @@ import { userReducer, signUp } from '../../../redux/user/user';
 const hitAPIWithSignupDetails = (details) => {
   const { name, email } = details;
 
+  return userReducer({}, signUp({
+    name,
+    email,
+    loggedIn: '',
+    userId: 1,
+    signedUp: true,
+  }));
 };
 
 export default hitAPIWithSignupDetails;
