dc4ccde6bb10b4d1a4d18b7548e2f59d3a991110
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:26:28 +0100

    Implement logout side effect test

--- src/tests/authentication_reducers/__mocks__/hitAPIWithLogoutDetails.js
+++ src/tests/authentication_reducers/__mocks__/hitAPIWithLogoutDetails.js
@@ -3,6 +3,13 @@ import { userReducer, logout } from '../../../redux/user/user';
 const hitAPIWithLogoutDetails = (details) => {
   const { auth } = details;
 
+  return userReducer({}, logout({
+    name: '',
+    email: '',
+    loggedIn: !auth || 'out',
+    userId: '',
+    signedUp: false,
+  }));
 };
 
 export default hitAPIWithLogoutDetails;
