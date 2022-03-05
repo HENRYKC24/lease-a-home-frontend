16a6f7db8c0333e33ffbb66cf7616eb93d0495a4
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:25:57 +0100

    Add logout side effect test structure

---
+++ src/tests/authentication_reducers/__mocks__/hitAPIWithLogoutDetails.js
@@ -0,0 +1,8 @@
+import { userReducer, logout } from '../../../redux/user/user';
+
+const hitAPIWithLogoutDetails = (details) => {
+  const { auth } = details;
+
+};
+
+export default hitAPIWithLogoutDetails;
