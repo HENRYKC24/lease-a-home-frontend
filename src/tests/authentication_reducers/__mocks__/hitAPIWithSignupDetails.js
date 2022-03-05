9b57434c77acc8999e4224bbe2b1af3c97da0520
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:28:15 +0100

    Add sign up side effect test structure

---
+++ src/tests/authentication_reducers/__mocks__/hitAPIWithSignupDetails.js
@@ -0,0 +1,8 @@
+import { userReducer, signUp } from '../../../redux/user/user';
+// import { Dispatch } from "react";
+const hitAPIWithSignupDetails = (details) => {
+  const { name, email } = details;
+
+};
+
+export default hitAPIWithSignupDetails;
