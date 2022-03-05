747a3f2c508fcd44e418c44d2bf475eae5143a39
HENRY-KC <henrykc24@yahoo.com>
2022-03-05 16:10:17 +0100

    Add mock for the router in the login page test

--- src/tests/authentication/loginTest.test.js
+++ src/tests/authentication/loginTest.test.js
@@ -4,6 +4,13 @@ import renderer from 'react-test-renderer';
 import Logout from '../../pages/Logout';
 import store from '../../redux/configureStore';
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
 it('Login renders the UI as expected', () => {
   const tree = renderer.create(
     <Provider store={store}>
