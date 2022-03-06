11d7148d931cda5617b0c404a101d2177ca3fbfa
wuyepabdul <wuyepabdul@gmail.com>
2022-03-06 23:55:24 +0100

    Display lease details

--- src/pages/MyLeaseDetails.js
+++ src/pages/MyLeaseDetails.js
@@ -21,65 +21,71 @@ const MyLeaseDetails = () => {
 
   return (
     <div className="container">
-      {error ? 'error' : console.log('error', error) }
-      {loading ? 'loading' : console.log('lease', lease) }
-      {/* <div className=" card mb-3 mt-5">
-        <div className="row g-0">
-          <div className="col-md-5">
-            <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="h-100 img-fluid rounded-start" alt="..." />
-            {' '}
+      {error && (
+      <p className="alert alert-danger">
+        {error}
+      </p>
+      ) }
+      {loading ? '' : console.log(lease.apartment)}
+      {loading ? 'loading' : (
+        <div className=" card mb-3 mt-5">
+          <div className="row g-0">
+            <div className="col-md-5">
+              <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="h-100 img-fluid rounded-start" alt="..." />
+              {' '}
 
-          </div>
-          <div className="col-md-7">
-            <div className="card-body">
-              <h5 className="card-title text-center">
-                {lease.name && lease.name}
-                { leaseDetails.name && leaseDetails.name}
-                Apartment Name
-              </h5>
-              <p className="text-center">Lorem Ipsum Depricano lasander Lorem Ipsum Depricano </p>
+            </div>
+            <div className="col-md-7">
+              <div className="card-body">
+                <h5 className="card-title text-center">
+                  {lease.name && lease.name}
+                  { leaseDetails.name && leaseDetails.name}
+                  Apartment Name
+                </h5>
+                <p className="text-center">Lorem Ipsum Depricano lasander Lorem Ipsum Depricano </p>
 
-              <table className="table">
-                <tbody>
-                  <tr>
-                    <td />
-                    <td>City</td>
-                    <td> - </td>
-                    <td />
-                    <td>Jos</td>
-                  </tr>
-                  <tr>
-                    <td />
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td />
+                      <td>City</td>
+                      <td> - </td>
+                      <td />
+                      <td>Jos</td>
+                    </tr>
+                    <tr>
+                      <td />
 
-                    <td>Monthly Rent</td>
-                    <td> - </td>
-                    <td />
-                    <td>$500</td>
-                  </tr>
-                  <tr>
-                    <td />
-                    <td>Maintenance Fee</td>
-                    <td> - </td>
-                    <td />
-                    <td>$100</td>
-                  </tr>
-                  <tr>
-                    <td />
+                      <td>Monthly Rent</td>
+                      <td> - </td>
+                      <td />
+                      <td>$500</td>
+                    </tr>
+                    <tr>
+                      <td />
+                      <td>Maintenance Fee</td>
+                      <td> - </td>
+                      <td />
+                      <td>$100</td>
+                    </tr>
+                    <tr>
+                      <td />
 
-                    <td>Expiry Date</td>
-                    <td> - </td>
-                    <td />
-                    <td>20th October, 2022</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="d-flex justify-content-end">
-                <button type="button" className="btn btn-warning">Unlease</button>
+                      <td>Expiry Date</td>
+                      <td> - </td>
+                      <td />
+                      <td>20th October, 2022</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="d-flex justify-content-end">
+                  <button type="button" className="btn btn-warning">Unlease</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div> */}
+      )}
 
       <h2 className="text-center text-info  my-5">Interiors</h2>
       <div className="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-xs-2 g-2 g-lg-3 mb-5 ">
