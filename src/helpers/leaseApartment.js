3e0b1066af5184fc7974fd11d7f7ece7cfe4bbb0
wuyepabdul <wuyepabdul@gmail.com>
2022-03-06 21:13:49 +0100

    Fetch data from apartment endpoint

--- src/helpers/leaseApartment.js
+++ src/helpers/leaseApartment.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const getLeaseApartment = (id) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+export const getLeaseApartment = async (id) => {
+  try {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/apartments`);
+  } catch (error) {
+    console.log('error');
+  }
+};
