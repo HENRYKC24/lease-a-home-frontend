2af10c13b0ae341668f935a250134741a1df18ff
wuyepabdul <wuyepabdul@gmail.com>
2022-03-06 21:26:44 +0100

    Return matched data

--- src/helpers/leaseApartment.js
+++ src/helpers/leaseApartment.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-export const getLeaseApartment = async (id) => {
+const getLeaseApartment = async (id) => {
   try {
     const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/apartments`);
+
+    const apartmentDetails = data.map((apartment) => apartment.id === id);
+    return apartmentDetails;
   } catch (error) {
     console.log('error');
   }
 };
+
+export default getLeaseApartment;
