f7533aa60ed37465e5fda13f1d6aa17e6dbce949
Whoistolu <digittolu25@gmail.com>
2022-03-06 22:03:24 +0100

    Add more styles to the home page

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-export const Home = () => {
+const Home = () => {
   const apartmentss = useSelector((state) => state.apartment);
   const { apartments } = apartmentss;
   console.log(apartments, 'hello');
@@ -20,11 +20,13 @@ export const Home = () => {
       apartments[0].map((item) => (
         <div key={item.id} className="col-12 col-md-6 col-lg-4">
           <Link style={{ textDecoration: 'none' }} to={`${item.id}`} className="m-3">
-            <img className="apartment-image center-block" src={item.image} alt={item.name} />
-            <h4 className="my-2 text-center apartmentname">{item.name}</h4>
-            <p className="description font-weight-light text-left ms-3">
-              {item.description}
-            </p>
+            <div className="card mx-3">
+              <img className="apartment-image hover_effect center-block" src={item.image} alt={item.name} />
+              <h4 className="my-2 text-center apartmentname">{item.name}</h4>
+              <p className="description font-weight-light text-center ms-3">
+                {item.description}
+              </p>
+            </div>
           </Link>
         </div>
       ))
