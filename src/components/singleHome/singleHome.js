857ee0c7e9c887c55058de6d0fe109b45fe278de
Whoistolu <digittolu25@gmail.com>
2022-03-07 20:57:53 +0100

    Change classname card to homepage-card

--- src/components/singleHome/singleHome.js
+++ src/components/singleHome/singleHome.js
@@ -4,7 +4,11 @@ import { useSelector } from 'react-redux';
 const Detail = () => {
   const apartmentss = useSelector((state) => state.apartment);
   const { apartment } = apartmentss;
-  const { name, description, image } = apartment;
+  const {
+    name, description, image, maintenance_fee: maintenance,
+    monthly_rent: rent, city, reservation_expiry_date: reservation,
+    created_at: created, updated_at: updated, interior,
+  } = apartment;
 
   if (!apartment) {
     return (
@@ -13,13 +17,54 @@ const Detail = () => {
   }
   return (
     <section>
-      <h1>Hello details</h1>
-      <div className="card mx-3">
-        <img className="apartment-image hover_effect center-block" src={image} alt={name} />
-        <h4 className="my-2 text-center apartmentname">{name}</h4>
-        <p className="description font-weight-light text-center ms-3">
-          {description}
-        </p>
+      <div className="row">
+        <div className="text-center mt-5">
+          <p>
+            Reservation-Expiry-Date:
+            {' '}
+            {reservation}
+          </p>
+          <p>
+            Created-At:
+            {' '}
+            {created}
+          </p>
+          <p>
+            Updated-At:
+            {' '}
+            {updated}
+          </p>
+          <p>
+            Maintenance-Fee:
+            {' '}
+            {maintenance}
+          </p>
+          <p>
+            Monthly-Rent:
+            {' '}
+            {rent}
+          </p>
+          <p>
+            City:
+            {' '}
+            {city}
+          </p>
+          <button className="btn btn-success book-apartment my-4" type="button">Book Apartment</button>
+        </div>
+        <div className="singlecard">
+          <img className="singleapartment-image m-3" src={image} alt={name} />
+          <h4 className="my-2 text-center apartmentname">{name}</h4>
+          <p className="singledescription font-weight-light text-center ms-3">
+            {description}
+          </p>
+        </div>
+      </div>
+      <div className="row m-3">
+        {interior && interior.map((feature) => (
+          <div key={feature.id}>
+            <img className="feature" src={feature} alt={name} />
+          </div>
+        ))}
       </div>
     </section>
   );
