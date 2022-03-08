8c7409125f44b11062690ba1e903dc2c327027af
wuyepabdul <wuyepabdul@gmail.com>
2022-03-08 23:02:01 +0100

    Create modal

--- src/pages/MyLeases.js
+++ src/pages/MyLeases.js
@@ -38,6 +38,29 @@ const MyLeases = () => {
     dispatch(deleteLeaseAction(id));
     window.location.reload();
   };
+
+  const deleteModal = (leaseId) => (
+    <>
+      <button type="button" onClick={() => handleSetId(leaseId)} className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Delete
+      </button>
+
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title text-center" id="exampleModalLabel">Are You Sure?</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" onClick={() => handleDeleteLease(id)} className="btn btn-danger">Yes, Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
   
   return (
     user ? (
