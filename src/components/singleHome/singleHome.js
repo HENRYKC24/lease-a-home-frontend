bf82b45c8d65cb5fce37697982cf7bb008403d5a
HENRY-KC <henrykc24@yahoo.com>
2022-03-08 18:13:45 +0100

    Fix sigle home reload issue

--- src/components/singleHome/singleHome.js
+++ src/components/singleHome/singleHome.js
@@ -7,12 +7,14 @@ const Detail = () => {
   const location = useLocation();
   const { id } = location.state;
   const apartment = apartmentss.apartments;
-  const apartmentMain = apartment[0].filter((one) => one.id === id);
+  const apartmentMain = apartment.length && apartment[0].filter((one) => one.id === id);
   const {
     name, description, image, maintenance_fee: maintenance,
     monthly_rent: rent, city, reservation_expiry_date: reservation,
     interior,
-  } = apartmentMain[0];
+  } = apartmentMain[0] || {
+    name: '', description: '', image: '', maintenance_fee: '', monthly_rent: '', reservation_expiry_date: '', interior: [],
+  };
 
   if (!apartment) {
     return (
@@ -21,51 +23,53 @@ const Detail = () => {
   }
 
   return (
-    <section>
-      <div>
+    apartmentMain ? (
+      <section>
+        <div>
 
-        <div className="singlecard">
-          <img className="singleapartment-image" src={image} alt={name} />
-          <h4 className="my-2 text-center apartmentname">{name}</h4>
-          <p className="singledescription font-weight-light text-center ms-3">
-            {description}
-          </p>
+          <div className="singlecard">
+            <img className="singleapartment-image" src={image} alt={name} />
+            <h4 className="my-2 text-center apartmentname">{name}</h4>
+            <p className="singledescription font-weight-light text-center ms-3">
+              {description}
+            </p>
 
-          <div className="text-center mt-5">
-            <p>
-              Reservation-Expiry-Date:
-              {' '}
-              {reservation}
-            </p>
-            <p>
-              Maintenance-Fee:
-              {' '}
-              {maintenance}
-            </p>
-            <p>
-              Monthly-Rent:
-              {' '}
-              {rent}
-            </p>
-            <p>
-              City:
-              {' '}
-              {city}
-            </p>
-            <button className="btn btn-success book-apartment my-4" type="button">
-              <Link style={{ textDecoration: 'none', color: 'white' }} to="/lease_form">Book Apartment</Link>
-            </button>
+            <div className="text-center mt-5">
+              <p>
+                Reservation-Expiry-Date:
+                {' '}
+                {reservation}
+              </p>
+              <p>
+                Maintenance-Fee:
+                {' '}
+                {maintenance}
+              </p>
+              <p>
+                Monthly-Rent:
+                {' '}
+                {rent}
+              </p>
+              <p>
+                City:
+                {' '}
+                {city}
+              </p>
+              <button className="btn btn-success book-apartment my-4" type="button">
+                <Link style={{ textDecoration: 'none', color: 'white' }} to="/lease_form">Book Apartment</Link>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="interior-card">
-        {interior && interior.map((feature) => (
-          <div key={feature.id}>
-            <img className="feature" src={feature} alt={name} />
-          </div>
-        ))}
-      </div>
-    </section>
+        <div className="interior-card">
+          {interior && interior.map((feature) => (
+            <div key={Math.random()}>
+              <img className="feature" src={feature} alt={name} />
+            </div>
+          ))}
+        </div>
+      </section>
+    ) : <p>Please, wait...</p>
   );
 };
 
