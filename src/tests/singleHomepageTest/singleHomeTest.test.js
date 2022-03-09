409430638fdd6b6902e62882a9e01278e64dbc5f
Whoistolu <digittolu25@gmail.com>
2022-03-09 13:25:29 +0100

    Write unit test for singlehome

---
+++ src/tests/singleHomepageTest/singleHomeTest.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import store from '../../redux/configureStore';
+import Detail from '../../components/singleHome/singleHome';
+
+configure({ adapter: new Adapter() });
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/another-route',
+    search: '',
+    hash: '',
+    state: null,
+    key: '5nvxpbdafa',
+  }),
+}));
+
+describe('App component', () => {
+  it('Renders correctly', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Detail />
+      </Provider>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
