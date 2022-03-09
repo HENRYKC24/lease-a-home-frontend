1cd4e1046d90a744e4ba8bb3c098998718a7d750
Whoistolu <digittolu25@gmail.com>
2022-03-09 13:33:41 +0100

    Unit test for the single apartment page

--- src/tests/singleHomepageTest/singleHomeTest.test.js
+++ src/tests/singleHomepageTest/singleHomeTest.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import store from '../../redux/configureStore';
 import Detail from '../../components/singleHome/singleHome';
 
