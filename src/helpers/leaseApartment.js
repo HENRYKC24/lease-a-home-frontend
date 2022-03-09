a057703aabfde980e3edbd1742c4a83b4bc3fc0e
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-03-09 15:15:29 +0200

    Remove console.log

--- src/helpers/leaseApartment.js
+++ src/helpers/leaseApartment.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 // eslint-disable-next-line consistent-return
 const getLeaseApartment = async (id) => {
   try {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL2}/apartments`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/apartments`);
     const apartmentDetails = data.find((apartment) => apartment.id === id);
-    console.log('apartmentDetails', apartmentDetails);
     return apartmentDetails.name;
   } catch (error) {
     console.log('error');
