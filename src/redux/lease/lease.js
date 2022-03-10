dc0e6cb80e044cd3e5bb34d1a5059a6f467fb05f
HENRY-KC <henrykc24@yahoo.com>
2022-03-10 22:11:16 +0100

    Update lease.js

--- src/redux/lease/lease.js
+++ src/redux/lease/lease.js
@@ -15,7 +15,7 @@ const DELETE_LEASE_FAIL = 'DELETE_LEASE_FAIL';
 const CREATE_LEASE = 'lease_a_home/CREATE_LEASE';
 const LEASE_STATUS = 'lease_a_home/LEASE_STATUS';
 
-const baseUrl = process.env.REACT_APP_BASE_URL;
+const baseUrl = process.env.REACT_APP_BASE_URL2;
 
 const initialState = {
   lease_status: '',
