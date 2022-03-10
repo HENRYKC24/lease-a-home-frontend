3beb8b7c69b8ac8269d53dd4a81864079fbb55fa
HENRY-KC <henrykc24@yahoo.com>
2022-03-10 22:09:32 +0100

    Remove hardcoded code in api.js

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://lease-a-home-api.herokuapp.com';
+const baseUrl = process.env.REACT_APP_BASE_URL2;
 
 const fetchDataApartments = async () => {
   try {
