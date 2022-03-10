a5d40a8c65d6f6f01dd05a88ca58612d6309e506
wuyepabdul <wuyepabdul@gmail.com>
2022-03-10 21:24:46 +0100

    Import persistLogin in single home page

--- src/components/singleHome/singleHome.js
+++ src/components/singleHome/singleHome.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import { login } from '../../redux/user/user';
+import persistLogin from '../../helpers/persistLogin';
 
 const Detail = () => {
   const dispatch = useDispatch();
